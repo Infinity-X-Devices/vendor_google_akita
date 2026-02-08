a2102d5fd478f5485a6ad3e55b89caaf1434cc54
Libra420T <ryantf420@gmail.com>
2026-02-08 09:59:58 -0500

    Vendor: Generate BP4A Akita

---
+++ Android.mk
@@ -0,0 +1,21 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),akita)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,b65b482017db29acd0dfb17cfee049c7d3042048)
+$(call add-radio-file-sha1-checked,radio/bl1.img,b2e7f70459e35f7ecfd526dbf3254d1e3e48442d)
+$(call add-radio-file-sha1-checked,radio/bl2.img,b18391697d8fe35b37927716b5708acf13770002)
+$(call add-radio-file-sha1-checked,radio/bl31.img,61f5c81e5d0c8f45bb959d4580f559395610650e)
+$(call add-radio-file-sha1-checked,radio/gcf.img,9ec567bba3b07e9b1ac85e4062a1a4c8d50b6cd9)
+$(call add-radio-file-sha1-checked,radio/gsa.img,4d8b4fcac7ce9523df1caee139dee7674bf05ecd)
+$(call add-radio-file-sha1-checked,radio/gsa_bl1.img,6235bc3b368b38772774ad292f574701f5e5c434)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,31df7d6f30165303b06eb986f5f9a91ebb995ff8)
+$(call add-radio-file-sha1-checked,radio/modem.img,2ba39185d881978936749332b8939ae2f15409c2)
+$(call add-radio-file-sha1-checked,radio/pbl.img,909e5aa6af69bd55cf7ce27369609a324aef0599)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,eed4bfd7f3dfbe3a6bf8130a77cb0483429909c9)
+
+endif
